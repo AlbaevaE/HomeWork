3c4129ee6223c75b190506fec29d0693e275f5c1
elvira <aelvira312@gmail.com>
2019-02-07 18:47:48 +0600

    servlet

--- src/main/webapp/h1/MyHomeWork.java
+++ src/main/webapp/h1/MyHomeWork.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.util.Random;
 
 public class MyHomeWork extends HttpServlet {
     @Override
@@ -12,9 +13,25 @@ public class MyHomeWork extends HttpServlet {
         PrintWriter out = resp.getWriter();
         req.getRequestDispatcher("test.jsp").forward(req,resp);
         out.println("<html>");
-        out.println("<head> c</head>");
-        out.println("<body> %=" + new Date() + "%</body>");
-        out.println("</html>"); // не пойму как правильно вывести
+        out.println("<head> time-date </head>");
+        out.println("<body> <p>" + new Date() + "</p></body>");
+        out.println("</html>"); // сервлет со временем работает, после добавления второго выдает ошибку 500
+
+        Random rm = new Random();
+        int sum = 0;
+        int max =100;
+        int min =1;
+        int a = rm.nextInt((max-min)+min+1);
+        out.println("<html>");
+        out.println("<head> sum </head>");
+        out.println("body>  %=" +(sum+=a)+"%</body>");
+        out.println("</html>");
+
+        out.println("html>");
+        out.println("<head> google </head>");
+        out.println("<body> %=www.google.com%</body>");
+        out.println("/html");
+
 
     }
 
