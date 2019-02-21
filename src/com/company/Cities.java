a22595b1b35e1fdddc2574f9bf45b5b9e33d4862
elvira <aelvira312@gmail.com>
2019-02-21 18:29:47 +0600

    hw51

---
+++ src/com/company/Cities.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Objects;
+
+public class Cities {
+    int id;
+    String name;
+
+    public Cities(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cities cities = (Cities) o;
+        return id == cities.id &&
+                Objects.equals(name, cities.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "id: " + id +
+                ", name: " + name ;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,61 @@
 package com.company;
 
-import java.awt.*;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BD bd = new BD();
-        bd.connect();
-        bd.insertCar(new Car(43, 1579));
-        bd.insertCarInfo(new CarInfo(11, 1556, "Bugatti Type 64 1939", 2987000, "Black"));
-        System.out.println(bd.getAll());
+        HashSet<Cities> set = new HashSet<>();
+        TreeSet<Cities> tree = new TreeSet<>();
+        LinkedList<Cities> city = new LinkedList<>();
+
+        city.add(new Cities(3948, "Ak-Suu"));
+        city.add(new Cities(3736, "Bazar-Korgan"));
+        city.add(new Cities(37, "Jalal-Abad"));
+        city.add(new Cities(3746, "Kara-Kul"));
+        city.add(new Cities(3745, "Tash-Kumyr"));
+        city.add(new Cities(32, "Osh"));
+
+//        HashMap<Integer, Cities> citiesHashMap = new HashMap<>();
+//        citiesHashMap.put(3948, new Cities("Ak-Suu"));
+//        citiesHashMap.put(3736, new Cities("Bazar-Korgan"));
+//        citiesHashMap.put(37, new Cities("Jalal-Abad"));
+//        citiesHashMap.put(3746, new Cities("Kara-Kul"));
+//        citiesHashMap.put(3745, new Cities("Tash-Kumyr"));
+//        citiesHashMap.put(32, new Cities("Osh"));
+//        int s = 0;
+//        Set<Integer> key = citiesHashMap.keySet();
+//        //   System.out.println(citiesHashMap.keySet());
+//       Iterator<Integer>it = key.iterator();
+//       while (it.hasNext()){
+//           if (key % 2 == 0) {
+//                s=i;
+//
+//            }
+//        }
+
+
+        for (int i = 0; i < city.size(); i++) {
+            if (city.get(i).name.length() > 5) {
+                city.remove(i);
+            }
+        }
+
+        for (int i = 0; i < city.size(); i++) {
+            if (city.get(i).id % 2 != 0) {
+                tree.add(city.get(i));
+            } else {
+                set.add(city.get(i));
+            }
+        }
+
+        System.out.println("TreeSet: "+tree);
+        System.out.println("Set: "+set);
+        Iterator<Cities> it = ((TreeSet<Cities>) tree).descendingIterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
 
     }
 }
-//    create table car(                     Так я создала таблицы. Долго думала,
-//        id serial primary key,               положить информацию в машину или машину в информацию.
-//        num int unique,
-//        info int references car_info(id));
-//
-//        create table car_info(
-//        id int primary key,
-//        year_of_issue int not null,
-//        model varchar(50)not null,
-//        price int not null,
-//        color varchar(40)not null);
-//
-//        select * from car_info
